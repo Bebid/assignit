bf7a190090f53132e92d76d19e31c9a598cea852
John Apostol <bebid28@gmail.com>
2025-07-29 00:13:25 +0800

    add(task create): form submit event

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Button.css";
 
-function Button({ type = "primary", size = "regular", children }) {
-    return <button className={`button ${type} ${size}`}>{children}</button>;
+function Button({
+    type = "primary",
+    size = "regular",
+    children,
+    submit = false,
+}) {
+    return (
+        <button
+            type={`${submit ? "submit" : "button"}`}
+            className={`button ${type} ${size}`}
+        >
+            {children}
+        </button>
+    );
 }
 
 export default Button;
--- src/components/Tasks/CreateForm.js
+++ src/components/Tasks/CreateForm.js
@@ -6,15 +6,19 @@ import Input from "../Form/Input";
 import Button from "../Button";
 
 function CreateFrom() {
+    function formSubmitted(event) {
+        event.preventDefault();
+        alert("test");
+    }
     return (
-        <form className="create-form">
+        <form className="create-form" onSubmit={formSubmitted}>
             <h2>Create Form</h2>
             <Input id="assignee" label="Assignee"></Input>
             <Input id="title" label="Title"></Input>
             <Textarea id="description" label="Description"></Textarea>
             <div className="form-footer">
                 <Button type="secondary">Cancel</Button>
-                <Button>Save</Button>
+                <Button submit={true}>Save</Button>
             </div>
         </form>
     );
