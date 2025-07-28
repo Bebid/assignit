bae628da212ff5d7a093a94b4ed6d240dc0a466c
John Apostol <bebid28@gmail.com>
2025-07-28 12:06:39 +0800

    udpate: Login page UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,8 @@ import "../css/login.css";
 import google from "../images/google.svg";
 import React from "react";
 
+import Button from "../components/Button";
+
 const Login = () => {
     return (
         <section id="login">
@@ -11,9 +13,9 @@ const Login = () => {
                     <div class="card-header font-cursive">Assign.It</div>
                     <div class="card-body">
                         <h1>Hello, Welcome!</h1>
-                        <button class="button">
+                        <Button type="secondary">
                             <img src={google}></img>Login with Google
-                        </button>
+                        </Button>
                     </div>
                 </div>
             </div>
